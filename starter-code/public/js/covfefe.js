9b5844adbeba17bfd8e451fd8d25eeee9bbb2220
Deokjae Jeon <deokjj@github.com>
2017-06-21 18:03:27 -0400

    Done

---
+++ starter-code/public/js/covfefe.js
@@ -0,0 +1,6 @@
+const covfefe = document.querySelector('img.covfefe');
+const meaning = document.querySelector("img.meaning");
+
+covfefe.addEventListener('click', ()=>{
+  meaning.style.visibility = "visible";
+});
